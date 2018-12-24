28d29ee4fd7ab3fb5bd37463953d411eddca0d9a
Vitor Rigoni <vitorrigoni@yahoo.com.br>
2018-12-23 16:52:50 -0800

    Adds samples for nullable reference types

---
+++ 001-NullableReferenceTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _001_NullableReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = null;
+
+            Console.WriteLine($"Length of myString is: {myString.Length}");
+        }
+    }
+}

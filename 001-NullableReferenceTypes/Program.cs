5df2447aef494498f7b0cbe150a7747a00c3bb6c
Vitor Rigoni <vitorrigoni@yahoo.com.br>
2018-12-23 16:58:09 -0800

    Adds sample explanation

--- 001-NullableReferenceTypes/Program.cs
+++ 001-NullableReferenceTypes/Program.cs
@@ -4,9 +4,17 @@ namespace _001_NullableReferenceTypes
 {
     class Program
     {
+        /*
+            This example throws a null reference exception.
+            The important part of this example is to demonstrate
+            how C#8 shows warnings indicating possible null reference exceptions
+            unless you explicitly tell the compiler that you are expecting null.
+
+            Note that this won't fail the build.
+        */
         static void Main(string[] args)
         {
-            string myString = null;
+            string myString = null; 
 
             Console.WriteLine($"Length of myString is: {myString.Length}");
         }
